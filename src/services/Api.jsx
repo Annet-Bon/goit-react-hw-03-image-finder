09afcc58eb463a5c6a966063a1ce77d1d4fc38f9
Annet-Bon <66079586+Annet-Bon@users.noreply.github.com>
2021-02-16 20:05:35 +0200

    popandos

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const fetchPicturesWithQuery = (searchQuery, page = 1) => {
-	return axios
+	axios
 		.get(
 			`https://pixabay.com/api/?key=19267083-f7a103736789b336d201af38a&q=${searchQuery}&image_type=photo&per_page=12&page=${page}`
 		)
 		.then((response) => response.data.hits);
 };
 
-export default {
-	fetchPicturesWithQuery,
-};
+export default fetchPicturesWithQuery;
